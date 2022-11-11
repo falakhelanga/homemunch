0ad21d189de7d3f1d3efd75e62991e502bf93c85
Falakhe <falakhe@Marks-MacBook-Pro-M1.local>
2022-11-11 23:33:54 +0200

    [X]

--- components/app-layout/TopNavBar/index.tsx
+++ components/app-layout/TopNavBar/index.tsx
@@ -1,14 +1,21 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import React from "react";
 import Body from "../../elements/Body";
 
 const TopNavBar = () => {
   return (
-    <div className="h-[12vh]  w-screen  py-4 fixed ">
-      <Body>
+    <div className="h-[12vh]  w-screen  py-4 fixed z-10 ">
+      <Body className="flex justify-between items-center">
         <div className="md:h-[6rem] md:w-[6rem] w-[4rem] h-[4rem] ">
           <img src="/logo.jpeg" alt="logo" className="rounded-full " />
         </div>
+        <FontAwesomeIcon
+          icon={faBars}
+          className="text-white md:cursor-pointer"
+          size="lg"
+        />
       </Body>
     </div>
   );
