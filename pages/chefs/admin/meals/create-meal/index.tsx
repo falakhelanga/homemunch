fbffd640f6c258ab16a3bcdbb930ca61269b8467
Falakhe <falakhe@Marks-MacBook-Pro-M1.local>
2022-12-06 06:41:48 +0200

    [X]

--- pages/chefs/admin/meals/create-meal/index.tsx
+++ pages/chefs/admin/meals/create-meal/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import AdminPageHeader from "../../../../../components/elements/AdminPageHeader";
 
-import CreateMeal from "../../../../../components/sections/chefs/meals/CreateMeal";
+// import CreateMeal from "../../../../../components/sections/chefs/meals/CreateMeal";
 
 const CreateMealPage = () => {
   return (
     <div>
       <AdminPageHeader withBack title="Create Dish" />
       <div className="px-[6rem] h-[85vh] overflow-auto">
-        <CreateMeal />
+        {/* <CreateMeal /> */}
       </div>
     </div>
   );
